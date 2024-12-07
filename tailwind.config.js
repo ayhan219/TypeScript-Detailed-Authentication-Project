ecf9fdd8eb4b53c70efc7546a991de6591772df6
Ayhan <ayhan-games@hotmail.com>
2024-12-07 13:45:47 +0300

    tailwind.config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        slideDown:{
+          "0%": { transform: "translateY(-100%)", opacity: "0" },
+          "100%": { transform: "translateY(0%)", opacity: "1" },
+        }
+      },
+      animation: {
+        slideDown: "slideDown 0.5s ease-in-out forwards",
+        slideUp: "slideUp 0.5s ease-in-out forwards"
+      },
+    },
   },
   plugins: [],
 }
